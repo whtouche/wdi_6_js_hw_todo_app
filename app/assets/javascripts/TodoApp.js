20e6dbc1da8be86d08e78b2ea0c6730ed9f16f33
Alex Grant <grantovich@users.noreply.github.com>
2014-05-30 10:34:45 -0400

    Create sort buttons dynamically

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -1,6 +1,7 @@
 var TodoApp = {
   todos: [],
   sorts: { pending: 'date', completed: 'date' },
+  sortProperties: ['date', 'name'],
 
   itemSubmitted: function(event){
     try {
@@ -28,13 +29,32 @@ var TodoApp = {
 
   sortChanged: function(event){
     var button = $(event.target);
-    button.parent().find('button').toggleClass('active');
-    this.sorts[button.data('list')] = button.data('sort');
+    button.parents('.sort-buttons').find('button').toggleClass('active');
+    this.sorts[button.parents('.todo-list').data('list')] = button.data('sort');
     this.updateLists();
   },
 
+  initSortButtons: function(){
+    var sorts = this.sorts;
+    var sortProperties = this.sortProperties;
+
+    Object.keys(sorts).forEach(function(status){
+      var buttons = $('[data-list="' + status + '"] .sort-buttons');
+
+      sortProperties.forEach(function(property){
+        var button = $('<button>').
+          attr('type', 'button').
+          addClass('btn btn-default').
+          attr('data-sort', property).
+          text(property);
+        if(sorts[status] === property){ button.addClass('active'); }
+        buttons.append(button);
+      });
+    });
+  },
+
   updateLists: function(){
-    $('.todo-list').empty();
+    $('.todos').empty();
     var sorts = this.sorts;
     var todos = this.todos;
 
@@ -43,7 +63,7 @@ var TodoApp = {
         filter(function(todo){ return todo.status() === status; }).
         sort(function(a, b){ return a[sorts[status]]() < b[sorts[status]]() ? -1 : 1; });
 
-      var targetList = $('.todo-list[data-list="' + status + '"]');
+      var targetList = $('[data-list="' + status + '"] .todos');
       todosWithStatus.forEach(function(todo){ targetList.append(todo.html()); });
     });
   },
--- app/assets/javascripts/app_ready.js
+++ app/assets/javascripts/app_ready.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
+  TodoApp.initSortButtons();
   $('#new-todo').on('submit', $.proxy(TodoApp.itemSubmitted, TodoApp));
   $('#todo-lists').on('click', '.complete-todo', $.proxy(TodoApp.itemCompleted, TodoApp));
   $('#todo-lists').on('click', '.delete-todo', $.proxy(TodoApp.itemDeleted, TodoApp));
   $('[data-sort]').on('click', $.proxy(TodoApp.sortChanged, TodoApp));
-  $.each(TodoApp.sorts, function(list, sort){
-    $('[data-list="' + list + '"][data-sort="' + sort + '"]').addClass('active');
-  });
 
   $('#new-todo-name').focus();
 });
