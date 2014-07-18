3fbccd7b712bbf05cb9416541f9c54be13e8f4d1
Ben White <whtouche@gmail.com>
2014-07-18 11:20:58 -0400

    User can add plain text to do items to list.
    *Clicking item creates an item on the "Done" list
    *But this new done item does not have the value of the clicked item

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -1,3 +1,38 @@
 var TodoApp = {
+  initialize: function() {
+    // $('#create-todo-button').click(function(event) {
+    //   event.preventDefault();
+    // });
 
+    $('#todo-form').submit(this.addTodo);
+    $('#todo-list').on('click', 'li', this.markDone);
+  },
+
+
+  addTodo: function(event) {
+    var newTodo = $('#todo-field').val();
+
+    if(newTodo !== ''){
+      var newTodoItem = $('<li>').text(newTodo);
+      $('#todo-list').append(newTodoItem);
+      $('#todo-field').val('');
+    }
+    event.preventDefault();
+  },
+  markDone: function() {
+    var newDone = $(this).val();
+    var newDoneItem = $('<li>').text(newDone);
+    $('#done-list').append(newDoneItem);
+  }
 };
+
+$(document).ready(function() { TodoApp.initialize(); });
+//Namespace / Class (like DemoApp)
+//Has a function which sorts lists alphabetically or by creation/completion date
+  //Called by click handler
+//Handles:
+  //Inserting
+  //Relocating
+  //Deleting
+  //Storing
+      //Array of TodoItem objects
--- app/assets/javascripts/TodoItem.js
+++ app/assets/javascripts/TodoItem.js
@@ -1,3 +1,10 @@
 var TodoItem = function(){
 
 };
+//Individual TodoItem
+//Handles:
+  //Constructing DOM elements for displaying itself
+  //Prevents blank items from being created (like validators in Rails)
+//Includes:
+  //'Complete' and 'Delete' buttons
+  //"created at" and "completed at", set at the appropriate points in code, and incorporate info into item's "rendered" DOM node
